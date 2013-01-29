48a733ced0ad587948e631298428e22b70f21525
Krasimir Nikolov <krasi.nikolov@gmail.com>
2013-01-29 20:50:10 +0200

    replace string

---
+++ C# Part - 2/09TextFiles/07RepalceString/RepalceString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ReplaceString
+{
+    static void Main()
+    {
+        List<string> container = new List<string>();
+        StreamReader reader = new StreamReader("..//..//input.txt");
+        StreamWriter writer = new StreamWriter("..//..//output.txt");
+        using (reader)
+        {
+            using (writer)
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    writer.WriteLine(line.Replace("start", "finish"));
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
