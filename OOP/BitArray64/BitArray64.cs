17ef79c88b8007f0149b7afc0fef5502fe654086
Krasimir Nikolov <krasi.nikolov@gmail.com>
2013-03-24 13:55:43 +0200

    BitArray64

---
+++ OOP/BitArray64/BitArray64.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    public class BitArray64 : IEnumerable<int>
+    {
+        public ulong Number { get; private set; }
+
+        public BitArray64(ulong number)
+        {
+            this.Number = number;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                int num = (int)(this.Number >> index) % 10;
+                return num & 1;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool isEqual = false;
+            BitArray64 objAs64 = obj as BitArray64;
+            if (objAs64 != null)
+            {
+                isEqual = objAs64.Number.Equals(this.Number);
+            }
+            return isEqual;
+        }
+        public override int GetHashCode()
+        {
+            return (int)(this.Number % 1000) ^ (int)(this.Number % 1000);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 63; i >= 0; i--)
+            {
+                yield return this[i];
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public static bool operator ==(BitArray64 first, BitArray64 second)
+        {
+            if (first.Number == second.Number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(BitArray64 first, BitArray64 second)
+        {
+            if (first.Number != second.Number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OOP/BitArray64/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 number = new BitArray64(555555555555550);
+            for (int i = 63; i >= 0; i--)
+            {
+                Console.Write(number[i]);
+            }
+            Console.WriteLine();
+            foreach (var item in number)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
